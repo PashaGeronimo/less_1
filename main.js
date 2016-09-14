7084b5cd31a7b79babf9222ba3a14d89ff60492e
PashaGeronimo <pasha@pasha.kiev.ua>
2016-09-14 18:04:58 +0300

    второй вариант решения

--- main.js
+++ main.js
@@ -1,54 +1,54 @@
 //Без приведения к числу
 
-// var a = prompt('Введите объем памяти айфона','');
-// var exch = prompt('Курс доллара?','27');
-// if (exch == null) a = null;
-// if (isNaN(exch)) a = 'd';
+var a = prompt('Введите объем памяти айфона','');
+var exch = prompt('Курс доллара?','27');
+if (exch == null) a = null;
+if (isNaN(exch)) a = 'd';
 
-// var cost;
-// switch (a) {
-//     case '8' :
-//         alert('Стоимость ' + exch * 600 + ' грн') ;
-//         break;
-//     case '16':
-//         alert('Стоимость ' + exch * 700 + ' грн') ;
-//         break;
-//     case '32':
-//         alert('Стоимость ' + exch * 800 + ' грн') ;
-//         break;
-//    case null:
-//         alert('Вы отменили запрос');
-//         break;
-//     default:
-//         alert( 'Нет такого объема памяти либо вы ввели не число');
-//         break;
-// }
+var cost;
+switch (a) {
+    case '8' :
+        alert('Стоимость ' + exch * 600 + ' грн') ;
+        break;
+    case '16':
+        alert('Стоимость ' + exch * 700 + ' грн') ;
+        break;
+    case '32':
+        alert('Стоимость ' + exch * 800 + ' грн') ;
+        break;
+   case null:
+        alert('Вы отменили запрос');
+        break;
+    default:
+        alert( 'Нет такого объема памяти либо вы ввели не число');
+        break;
+}
 
 
 //С приведением к числу, с точными ошибками
 
-var a = +prompt('Введите объем памяти айфона','');
-var exch = +prompt('Курс доллара?','27');
-if (isNaN(exch) || isNaN(a) ) a = 'f';
-if (exch == 0) a = 0;
-var cost;
-switch (a) {
-    case 8:
-        alert('Стоимость ' + exch * 600 + ' грн') ;
-        break;
-    case 16:
-        alert('Стоимость ' + exch * 700 + ' грн') ;
-        break;
-    case 32:
-        alert('Стоимость ' + exch * 800 + ' грн') ;
-        break;
-   case 0:
-        alert('Вы отменили запрос');
-        break;
-   case !isNaN(a) || a:
-        alert('Вы ввели не число');
-        break;
-    default:
-        alert( 'Нет такого объема памяти');
-        break;
-}
+// var a = +prompt('Введите объем памяти айфона','');
+// var exch = +prompt('Курс доллара?','27');
+// if (isNaN(exch) || isNaN(a) ) a = 'f';
+// if (exch == 0) a = 0;
+// var cost;
+// switch (a) {
+//     case 8:
+//         alert('Стоимость ' + exch * 600 + ' грн') ;
+//         break;
+//     case 16:
+//         alert('Стоимость ' + exch * 700 + ' грн') ;
+//         break;
+//     case 32:
+//         alert('Стоимость ' + exch * 800 + ' грн') ;
+//         break;
+//    case 0:
+//         alert('Вы отменили запрос');
+//         break;
+//    case !isNaN(a) || a:
+//         alert('Вы ввели не число');
+//         break;
+//     default:
+//         alert( 'Нет такого объема памяти');
+//         break;
+// }
